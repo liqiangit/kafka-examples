884c106111a52836f4c3e0aa9207c4b44a5d595f
liqiangit <liqiangit@163.com>
2018-03-04 18:31:03 +0800

    init

---
+++ src/main/java/com/liqiang/kafka/kafka_examples/App.java
@@ -0,0 +1,13 @@
+package com.liqiang.kafka.kafka_examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
